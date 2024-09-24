016f90069ea7d40f6ce61cd9596806794556df7b
ManojAle <156966858+ManojAle@users.noreply.github.com>
2024-09-23 23:59:04 -0400

    Add files via upload

---
+++ HW2/assets/recruitment.py
@@ -0,0 +1,10 @@
+# recruitment.py
+
+def hire():
+    print("Hire a new employee...")
+
+def fire():
+    print("Fire an existing employee...")
+
+def promote():
+    print("Promote an employee to a higher position...")
